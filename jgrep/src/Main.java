affd9896603d47418b8dcc92b8457fc8a476ed4e
allylavanda <allylavanda@aol.com>
2024-03-11 14:54:33 -0400

    initial commit

---
+++ jgrep/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * This program is made to be a windows equivalent of grep because I used windows subsystem for linux daily just for grep lol
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
