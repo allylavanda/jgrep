62fd3b7bcea9bfabb69694dc3909847282605e3d
allylavanda <allylavanda@aol.com>
2024-03-12 10:36:06 -0400

    Compartmentalization

---
+++ jgrep/src/CurrentDir.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Scanner;
+public class CurrentDir{
+    class MyFileNameFilter implements FilenameFilter {
+    String fname;
+    public MyFileNameFilter (String fName){
+        this.fname = fName;
+    }
+    public boolean accept(File dir, String name) {
+        return name.startsWith(fname);
+    }
+}
+    protected String dir;
+    protected String fileName;
+    private void getDir(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the directory you would like to search: ");
+        this.dir = sc.nextLine();
+        System.out.println("Dir: "+dir);
+        System.out.println("Enter the file name to search for: ");
+        this.fileName = sc.nextLine();
+        System.out.println("Content: "+fileName);
+        sc.close();
+    }
+    private void search(String dir, String fileName){
+        File directory = new File(dir);
+        MyFileNameFilter filter = new MyFileNameFilter(fileName);
+        String[] flist = directory.list(filter);
+
+        if(flist == null) {
+            System.out.println("Empty or non existing dir.");
+        } else {
+            for (int i = 0; i < flist.length; i++) {
+                System.out.println(dir+flist[i]+" found");
+            }
+        }
+    }
+    public void run(){
+        getDir();
+        search(this.dir, this.fileName);
+    }
+}
--- jgrep/src/Main.java
+++ jgrep/src/Main.java
@@ -1,9 +1,8 @@
 import java.io.File;
 import java.io.FilenameFilter;
-import java.util.Scanner;
 
 /**
- * This program is made to be a windows equivalent of grep because I used windows subsystem for linux daily just for grep lol
+ * This program is being made to be a windows equivalent of grep because I use windows subsystem for linux daily just for grep lol
  */
 class MyFileNameFilter implements FilenameFilter {
     String fname;
@@ -17,35 +16,19 @@ class MyFileNameFilter implements FilenameFilter {
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String dir, fileName;
         System.out.println("Welcome to jgrep by allylavanda");
-
-        // get directory and content/filename
-        System.out.println("Enter the directory you would like to search: ");
-        dir = sc.nextLine();
-        System.out.println("Dir: "+dir);
-        System.out.println("Enter the file name to search for: ");
-        fileName = sc.nextLine();
-        System.out.println("Content: "+fileName);
-
-
-        System.out.println("Searching...");
-        searchForFile(dir, fileName);
-        sc.close();
+        argHandler(args);
     }
 
-    private static void searchForFile(String dir, String fileName){
-        File directory = new File(dir);
-        MyFileNameFilter filter = new MyFileNameFilter(fileName);
-        String[] flist = directory.list(filter);
-
-        if(flist == null) {
-            System.out.println("Empty or non existing dir.");
-        } else {
-            for (int i = 0; i < flist.length; i++) {
-                System.out.println(dir+flist[i]+" found");
-            }
+    private static void argHandler(String[] args){
+        if(args.length == 0){
+            System.out.println("You must use arguments to run this application. If you need help, run this program with the 'help' argument, or consult the github.");
+            System.exit(0);
+        }
+        if(args[0].equals("cd")){
+            System.out.println("Starting current directory search...");
+            CurrentDir cur = new CurrentDir();
+            cur.run();
         }
     }
 }
