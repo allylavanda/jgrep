448c79bbbf59edb8e15f519961bfd35abfab50c0
allylavanda <allylavanda@aol.com>
2024-03-18 14:55:09 -0400

    Recursive Dir implementation

--- jgrep/src/RecursiveDir.java
+++ jgrep/src/RecursiveDir.java
@@ -1,15 +1,11 @@
 import java.io.File;
-import java.io.IOException;
-/** import java.io.FilenameFilter;
-import java.nio.file.Files; **/
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FilenameFilter;
 
 public class RecursiveDir {
     protected String dir;
     protected String fileName;
     
-    /** class MyFileNameFilter implements FilenameFilter {
+    class MyFileNameFilter implements FilenameFilter {
     String fname;
         public MyFileNameFilter (String fName){
         this.fname = fName;
@@ -17,62 +13,32 @@ public class RecursiveDir {
         public boolean accept(File dir, String name) {
         return name.toLowerCase().contains(fname);
         }
-    } **/
-    protected List<String> result = new ArrayList<String>();
-    private List<String> getResult(){
-        return result;
     }
-    
-    public void run(String dir, String fileName) throws IOException{
-        RecursiveDir recurse = new RecursiveDir();
-        recurse.setName(fileName);
-        recurse.setDir(dir);
-        recurse.searchDir(new File(recurse.getDir()), recurse.getName());
-        int x = recurse.getResult().size();
-        if(x == 0){
-            System.out.println("No results found.");
-        } else {
-            System.out.println("\nFound+" +x+ " results!\n");
-            for( String matched : recurse.getResult()){
-                System.out.println("Found: "+ matched );
-            }
-        }
-    }
-    private void searchDir(File dir, String fileName2) {
-        if(dir.isDirectory()){
-            search(dir);
-        } else {
-            System.out.println(dir.getAbsolutePath() + " does not exist!");
-        }
-    }
-    private void search(File dir) {
-        if (dir.isDirectory()){
-            if(dir.canRead()){
-                for( File tmp : dir.listFiles()){
-                    if(tmp.isDirectory()){
-                        search(tmp);
-                    } else {
-                        if(tmp.getName().toLowerCase().contains(getName())){
-                            result.add(tmp.getAbsolutePath().toString());
+    public void search(File directory){
+        MyFileNameFilter filter = new MyFileNameFilter(this.fileName);
+        String[] flist = directory.list(filter);
+        if(directory.isDirectory()){
+            if(directory.canRead()){
+                if(flist == null) {
+                    System.out.println("Empty or non existing dir.");
+                } else {
+                    for(File tmp : directory.listFiles()){
+                        if(tmp.isDirectory()){
+                            search(tmp);
+                        } else {
+                            for (int i = 0; i < flist.length; i++) {
+                                System.out.println(dir+"\\"+flist[i]+" found");
+                            }
                         }
                     }
-
+                    
                 }
             }
-        } else {
-            System.out.println("Denied.");
         }
     }
-    private void setName(String fName){
-        this.fileName = fName;
-    }
-    private void setDir(String dir){
+    public void run(String dir, String fileName){
         this.dir = dir;
-    }
-    private String getName(){
-        return fileName;
-    }
-    private String getDir(){
-        return this.dir;
+        this.fileName = fileName;
+        search(new File(this.dir));
     }
 }
